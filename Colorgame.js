2816a1fff562277428eeddda5a968c49892f6c20
thefreshprince98 <58113441+thefreshprince98@users.noreply.github.com>
2020-09-28 20:13:36 -0400

    change in the array changes the background color

--- Colorgame.js
+++ Colorgame.js
@@ -6,8 +6,22 @@ let colors = [
   "rgb(0, 0, 255)",
   "rgb(255, 0, 255)",
 ];
-
 var change = document.querySelectorAll(".boxes");
+
 for (let index = 0; index < change.length; index++) {
   change[index].style.backgroundColor = colors[index];
+  // click event
+  change[index].addEventListener("click",function (params) {
+
+    
+  })
 }
+
+var titleColor = document.querySelector("span");
+// picked color
+var pickedColor = colors[2];
+titleColor.innerText = pickedColor;
+// changes the color based on the array
+document.body.style.backgroundColor = pickedColor;
+// 
+ 
